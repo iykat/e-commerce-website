8603c7c6377c73ee672f0f17de6f3cde56a374c8
Prince Takyi <takyip28@gmail.com>
2022-05-15 21:24:10 +0000

    stripe integration

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/dist/client/link";
+import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
 
 import { Cart } from "./";
