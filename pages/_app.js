2f9ad149f4300e0789a869c73fbd114cbdab5314
Prince Takyi <takyip28@gmail.com>
2022-05-15 17:15:34 +0000

    finish cart component

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { Layout } from '../components';
-import '../styles/globals.css'
+import React from "react";
+import { Toaster } from "react-hot-toast";
+
+import { Layout } from "../components";
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-  <Component {...pageProps} />
-  </Layout>
-  )
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,20 +7,33 @@ import {
 } from "react-icons/ai";
 import { client, urlFor } from "../../LIB/client";
 import { Product } from "../../components";
+import { useStateContext } from "../../context/StateContext";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  };
+
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[index])} className="product-detail-image " />
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+            />
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
               <img
+                key={i}
                 src={urlFor(item)}
                 className={
                   i === index ? "small-image selected-image" : "small-image"
@@ -39,7 +52,6 @@ const ProductDetails = ({ product, products }) => {
               <AiFillStar />
               <AiFillStar />
               <AiFillStar />
-              <AiFillStar />
               <AiOutlineStar />
             </div>
             <p>(20)</p>
@@ -50,27 +62,30 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="num" onClick="">
-                <AiOutlineMinus />
-              </span>
-              <span className="plus" onClick="">
-                <AiOutlineMinus />
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}>
+                <AiOutlinePlus />
               </span>
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => onAdd(product, qty)}
+            >
               Add to Cart
             </button>
-            <button type="button" className="buy-now" onClick="">
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
         </div>
       </div>
+
       <div className="maylike-products-wrapper">
         <h2>You may also like</h2>
         <div className="marquee">
@@ -87,13 +102,14 @@ const ProductDetails = ({ product, products }) => {
 
 export const getStaticPaths = async () => {
   const query = `*[_type == "product"] {
-      slug {
-          current
-      }
+    slug {
+      current
     }
-    
-    `;
+  }
+  `;
+
   const products = await client.fetch(query);
+
   const paths = products.map((product) => ({
     params: {
       slug: product.slug.current,
@@ -109,10 +125,11 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
   const productsQuery = '*[_type == "product"]';
+
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
-  const bannerQuery = '*[_type == "banner"]';
-  const bannerData = await client.fetch(bannerQuery);
+
+  console.log(product);
 
   return {
     props: { products, product },
